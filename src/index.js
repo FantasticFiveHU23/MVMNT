c307c45e23d65004e71d66319ff64da8d3e4e820
iyinoluwamt <96902046+iyinoluwamt@users.noreply.github.com>
2023-02-13 17:31:59 -0500

    Removed redundant files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import App from "./pages/App";
+import './styles/App.css';
+
+import HomePage from "./pages/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const Main = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <div className="App">
+            <HomePage isLoggedIn={isLoggedIn} />
+        </div>
+    )
+}
+
 root.render(
   <React.StrictMode>
-      <App />
+      <Main/>
   </React.StrictMode>
 );

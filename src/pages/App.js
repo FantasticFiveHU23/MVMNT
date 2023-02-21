3193e5adc9f31e97113f6f1271ab7bf38c0e3f5b
James Fisher <61597295+jamesfisher19@users.noreply.github.com>
2023-02-21 02:31:46 -0500

    Feb 21 2:31 am

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,12 +4,14 @@ import {HomePage} from './HomePage';
 import Businesspage from './Businesspage';
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import {Header} from "../components/Header";
+import { LocalFinds } from './LocalFinds';
+import { Footer } from '../components/Footer';
 // import { Link } from 'react-router-dom';
 
 function App() {
   // eslint-disable-next-line
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  
   return (
     <div className="App">
       <Header />
@@ -18,8 +20,10 @@ function App() {
       <Routes>
       <Route path="/" element={<HomePage isLoggedIn={isLoggedIn} />} />
       <Route path = "/Businesspage" element={<Businesspage/>}/>
+      <Route path="/local-finds" element={<LocalFinds location="Washington D.C" />} />
       </Routes>
       </Router>
+      <Footer/>
     </div>
 
   );

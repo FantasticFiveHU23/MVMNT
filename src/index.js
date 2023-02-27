a64baa7320153e90a76b3f9b888a75a563dcf6bb
James Fisher <61597295+jamesfisher19@users.noreply.github.com>
2023-02-27 18:48:02 -0500

    Local-finds update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import awsExports from './aws-exports';
 import {Header} from "./components/Header";
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import BusinessPage from "./pages/BusinessPage";
+import {LocalFinds} from "./pages/LocalFinds";
 
 Amplify.configure(awsExports);
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -29,6 +30,7 @@ const Main = () => {
                         />}
                     />
                     <Route path = "/BusinessPage" element={<BusinessPage/>}/>
+                    <Route path="/local-finds" element={<LocalFinds location="Washington D.C" />} />
                 </Routes>
             </Router>
         </div>
